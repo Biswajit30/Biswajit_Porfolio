3a4642a1282798be0a39d7c7918603a14aa4c363
Biswajit30 <biswajitjena0611@gmail.com>
2023-08-13 21:53:42 +0530

    Updates

--- src/data/headerData.js
+++
@@ -1,9 +0,0 @@
-import resume from '../assets/pdf/Biswajit_React(2.4)Yr.nf.pdf';
-import  Biswajit  from '../jpg/newBisu.jpg'
-export const headerData = {
-    name: 'Biswajit Jena',
-    title: "Front End Developer (React.js)",
-    desciption:"Frontend Developer with 2+ Years of Professional Experience in User Interface Development with hands on Experience in JavaScript, React, TypeScript, Redux etc.Also Learning: React Native, NodeJs.",
-    image: Biswajit,
-    resumePdf: resume
-}
